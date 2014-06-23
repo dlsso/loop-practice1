b575e4e661d9f2c0ae09fd658260f08eb80471bd
dlsso <supertonesfan@hotmail.com>
2014-06-23 11:58:13 -0600

    Solved the problems

--- main.js
+++ main.js
@@ -9,3 +9,35 @@ console.log("Problem 2.")
 for(var i=1; i<=64; i = i*2) {
  console.log(i);
 }
+
+console.log("Problem 3.")
+
+for(var i=1; i<=3; i++) {
+ console.log(i,i,i);
+}
+
+console.log("Problem 4.")
+
+for(var i=0; i<=10; i=i+2) {
+ console.log(i);
+}
+
+console.log("Problem 5.")
+
+for(var i=3; i<=15; i=i+3) {
+ console.log(i);
+}
+
+console.log("Problem 6.")
+
+for(var i=9; i>=0; i--) {
+ console.log(i);
+}
+
+console.log("Problem 7.")
+
+for(var j=1; j<=3; j++){
+	for(var i=0; i<=3; i++) {
+	 console.log(i);
+	}
+}
